b29be295a67ef0cbbcea8fa4f46269a03fdd81d0
malnea1 <malnea1@students.towson.edu>
2016-09-23 01:47:59 -0400

    completion of Task 6

--- src/main/scala/edu/towson/cosc/cosc455/malnea1/lab4/Compiler.scala
+++ src/main/scala/edu/towson/cosc/cosc455/malnea1/lab4/Compiler.scala
@@ -1,17 +1,5 @@
 package edu.towson.cosc.cosc455.malnea1.lab4
 
-/**
-  * COSC 455 Programming Languages: Implementation and Design
-  * Lab 4 - A Simple Compiler
-  * Josh Dehlinger
-  * A simple compiler used for the simple English grammar in Section 2.2 of Adam Brooks Weber's
-  * "Modern Programming Languages" book. Parts of this code was adapted from Robert Sebesta's "Concepts
-  * of Programming Languages".
-  *
-  * This compiler assumes that the source file containing the sentences to parse are provided as the first
-  * runtime argument. Within the source file, the compiler assumes that each sentence to parse is provided
-  * on its own line.
-  */
 
 import scala.io.Source
 
@@ -26,7 +14,7 @@ object Compiler {
   def main(args: Array[String]) = {
 
     // get input file name from command line argument
-    val filename = args(0)
+    val filename: String = args(0)
 
     // for each line read from file, scan and parse
     for (line <- Source.fromFile(filename).getLines()) {
@@ -44,7 +32,6 @@ object Compiler {
       else
        println("The sentence '" + line + "' follows the BNF grammar.")
 
-      println
     }
   }
 
